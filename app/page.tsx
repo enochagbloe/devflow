a4b0ac772e575e4e9a69d416c1b22cabbac02b59
Enoch <enochagbloe16@gmail.com>
2025-02-17 04:17:33 +0000

    updates on set theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const page = () => {
   return (
-    <div>
-      <h1 className="bg-primary-500">Hello world</h1>
-    </div>
+    <>
+    
+<DropdownMenu>
+  <DropdownMenuTrigger>closed</     DropdownMenuTrigger>
+    <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+    </>
   );
 };
 
---
+++ context/theme.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ThemeProvider as NextThemeProvider, ThemeProviderProps } from 'next-themes'
+const ThemeProvider = ({children, ...props}: ThemeProviderProps) => {
+  return (
+      <NextThemeProvider {...props}>
+        {children}
+      </NextThemeProvider>
+
+  )
+}
+
+export default ThemeProvider
