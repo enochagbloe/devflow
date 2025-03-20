58b4fd23f5957838f6bebcfab7860bc04e8f69e3
Enoch <enochagbloe16@gmail.com>
2025-03-20 16:15:35 +0000

    Git providers

--- components/navigation/nav/nav.tsx
+++ components/navigation/nav/nav.tsx
@@ -1,26 +1,25 @@
+import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import Theme from "./theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12 ">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
+          alt="DevFlOW Logo"
           width={23}
           height={23}
-          alt="dev flow logo"
         />
-
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev <span className="text-primary-500">Flow</span>
+          Dev <span className="text-primary-500"> Flow </span>
         </p>
       </Link>
-      <p> Global Search</p>
-      <div className="flex-between gap-5">
-        <Theme/>
+      <p>Global search</p>
+      <div className="flex-between flex gap-5">
+        <Theme />
       </div>
     </nav>
   );
