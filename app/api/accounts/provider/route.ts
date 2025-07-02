f109b60a9e01de14ed9f42bd5af430c726e4acf1
Enoch <enochagbloe16@gmail.com>
2025-07-02 17:43:10 +0100

    Implement account by provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,26 @@
+import handleError from "@/lib/handler/error";
+import { ValidationError } from "@/lib/http.errors";
+import { AccountSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/globals";
+import User from "@/database/user.model";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    // extract the providerAccountId  from the request body
+    const { providerAccountId } = await request.json();
+    try {
+        // validate the account
+        const account = AccountSchema.partial().safeParse({providerAccountId});
+        // if not the account 
+        if(!account.success) throw new ValidationError(account.error.flatten().fieldErrors);
+        // if the account is valid
+        const user = await User.findOne({providerAccountId});
+        // if the account is not found
+        if(!user) throw new Error("account not found");
+        // if the account is found
+        return NextResponse.json({success: true, data: user}, {status: 200});
+    } catch (error) {
+        return handleError(error, "api") as APIErrorResponse;
+        
+    }
+}
